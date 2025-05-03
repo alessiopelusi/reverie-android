58655dae40fb1714547685990153ca3197cdee33
Beniamino Vagnarelli <beniaminovagnarelli@gmail.com>
2025-05-03 14:49:43 +0200

    "Working" diary page scroll

--- app/src/main/java/com/example/reverie/MainActivity.kt
+++ app/src/main/java/com/example/reverie/MainActivity.kt
@@ -4,23 +4,234 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.MailOutline
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import com.example.reverie.ui.theme.ReverieTheme
+import com.example.reverie.ui.theme.gialloScuro
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp()
+            ReverieTheme {
+                ScaffoldExample()
+            }
+        }
+    }
+}
+
+@Composable
+fun ScaffoldExample() {
+    var presses by remember { mutableIntStateOf(0) }
+
+    Scaffold(
+        topBar = { CustomTopBar() },
+        bottomBar = { CustomBottomAppBar() },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding).fillMaxSize().border(width = 2.dp, color = Color.Magenta, shape = RectangleShape),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Emotional Diary"
+            )
+
+            val listOfItems: List<String> = (1..10).map { "Item $it" }
+            val diaryPageListState = rememberLazyListState()
+            // start lazyrow from the end
+            LaunchedEffect(Unit) {
+                diaryPageListState.scrollToItem(listOfItems.lastIndex)
+            }
+
+            BoxWithConstraints (
+                modifier = Modifier.border(width = 2.dp, color = Color.Red, shape = RectangleShape).weight(1f, false),
+            ) {
+                LazyRow (
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    state = diaryPageListState,
+                    flingBehavior = rememberSnapFlingBehavior(lazyListState = diaryPageListState)
+                ) {
+                    itemsIndexed(listOfItems) { index, item ->
+                        Layout(
+                            content = {
+                                // Here's the content of each list item.
+                                val widthFraction = 0.90f
+                                DiaryPage(modifier = Modifier
+                                    .widthIn(max = LocalConfiguration.current.screenWidthDp.dp * widthFraction)
+                                    .aspectRatio(9f/16f),
+                                    item)
+                            },
+                            measurePolicy = { measurables, constraints ->
+                                // I'm assuming you'll declaring just one root
+                                // composable in the content function above
+                                // so it's measuring just the Box
+                                val placeable = measurables.first().measure(constraints)
+                                // maxWidth is from the BoxWithConstraints
+                                val maxWidthInPx = maxWidth.roundToPx()
+                                // Box width
+                                val itemWidth = placeable.width
+                                // Calculating the space for the first and last item
+                                val startSpace =
+                                    if (index == 0) (maxWidthInPx - itemWidth) / 2 else 0
+                                val endSpace =
+                                    if (index == listOfItems.lastIndex) (maxWidthInPx - itemWidth) / 2 else 0
+                                // The width of the box + extra space
+                                val width = startSpace + placeable.width + endSpace
+                                layout(width, placeable.height) {
+                                    // Placing the Box in the right X position
+                                    val x = if (index == 0) startSpace else 0
+                                    placeable.place(x, 0)
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+
+            /*
+            LazyRow(
+                modifier = Modifier.border(width = 2.dp, color = Color.Red, shape = RectangleShape).weight(1f, false),
+                contentPadding = PaddingValues(
+                    start = LocalConfiguration.current.screenWidthDp.dp / 10,
+                    end = LocalConfiguration.current.screenWidthDp.dp / 10
+                ),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                state = diaryPageListState,
+                flingBehavior = rememberSnapFlingBehavior(lazyListState = diaryPageListState)
+            ) {
+                items(listOfItems, key = String::hashCode) { label:String ->
+                    val widthFraction = 0.90f
+                    DiaryPage(modifier = Modifier
+                        .widthIn(max = LocalConfiguration.current.screenWidthDp.dp * widthFraction)
+                        .aspectRatio(9f/16f),
+                        label)
+                }
+            }
+            */
+
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Page 1/1",
+            )
+            Button(
+                onClick = {},
+                colors = ButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.primary,
+                    disabledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                    disabledContentColor = MaterialTheme.colorScheme.primary
+                ),
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Icon(Icons.Default.Edit, contentDescription = "Edit")
+            }
+        }
+    }
+}
+
+@Composable
+fun DiaryPage(modifier: Modifier, text: String) {
+    Box(
+        modifier = modifier
+            .border(width = 2.dp, color = Color.Blue, shape = RectangleShape)
+            .background(gialloScuro)
+    ) {
+        Text(text = text)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopBar() {
+    TopAppBar(
+        colors = topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(Icons.Rounded.Menu, contentDescription = "Menu")
+                Text("Reverie")
+                Icon(Icons.Rounded.Person, contentDescription = "Account")
+            }
+        }
+    )
+}
+
+@Composable
+fun CustomBottomAppBar() {
+    BottomAppBar (
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        contentColor = MaterialTheme.colorScheme.primary,
+    ) {
+        Row (
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Icon(
+                Icons.Outlined.Close,
+                contentDescription = "Menu",
+                modifier = Modifier.size(50.dp),
+            )
+            Icon(
+                Icons.Rounded.Home,
+                contentDescription = "Menu",
+                modifier = Modifier.size(50.dp)
+            )
+            Icon(
+                Icons.Outlined.MailOutline,
+                contentDescription = "Account",
+                modifier = Modifier.size(50.dp)
+            )
         }
     }
 }
@@ -53,8 +264,10 @@ fun ResizableImage() {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 500, heightDp = 1000)
 @Composable
 fun DefaultPreview() {
-    MyApp()
+    ReverieTheme {
+        ScaffoldExample()
+    }
 }
--- app/src/main/java/com/example/reverie/ui/theme/Theme.kt
+++ app/src/main/java/com/example/reverie/ui/theme/Theme.kt
@@ -14,13 +14,15 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    primaryContainer = Purple40
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    primaryContainer = Purple80
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,10 +43,10 @@ fun ReverieTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        /* dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+        } */
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
